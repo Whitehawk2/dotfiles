7299001bfa0f8f120581d3ada5b1f4b38dfa217d
whitehawk <whitehawk2@gmail.com>
2023-09-22 13:36:04 +0300

    install zsh
    trying an install zsh template for the before run script.
    should default to actual rpm/apt by very naive check

--- .chezmoiscripts/run_once_before_10_test.sh
+++ .chezmoiscripts/run_once_before_10_test.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo -e "\033[0;32m>>>>> this is a before script test!!! <<<<<\033[0m"
+
+if [ -n "$(command -v zsh)" ]; then
+    echo -e "\033[0;32m>>>>> installing zsh <<<<<\033[0m"
+    {{- if (and (eq .chezmoi.os "linux") (eq .chezmoi.osRelease.id "debian" "ubuntu")) -}}
+    sudo apt install -y zsh
+    {{- else if (and (eq .chezmoi.os "linux") (eq .chezmoi.osRelease.id "fedora" "suse" "nix")) -}}
+    sudo dnf install -y zsh
+    {{- else if (eq .chezmoi.os "darwin") -}}
+    brew install zsh
+    {{- end -}}
+    chsh -s $(which zsh)
+fi
